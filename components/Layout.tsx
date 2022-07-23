c7b1a8e0a1e7fd573fb5b7f0ef54228c92ffa91a
Jacek Grzybowski <totospl99@gmail.com>
2022-07-23 02:00:18 +0200

    basic setup

---
+++ components/Layout.tsx
@@ -0,0 +1,30 @@
+import Link from "next/link";
+
+type Props = {
+  children: JSX.Element;
+};
+
+export const Layout: React.FC<Props> = ({ children }) => {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <header className="bg-red-600 py-2 sticky top-0">
+        <div className="container mx-auto flex ">
+          <Link href="/">
+            <div className="flex items-center">
+              <img src="/hf_logo.png" className="max-h-14 p-1 flex-none" />
+              <h1 className="font-bold text-xl ">High Flyers</h1>
+            </div>
+          </Link>
+        </div>
+      </header>
+      <main className="container mx-auto flex-1 p-2 bg-slate-900 text-white">
+        {children}
+      </main>
+      <footer className="bg-fuchsia-100 py-4">
+        <div className="container mx-auto flex justify-center">
+          &copy; 2022 High Flyers
+        </div>
+      </footer>
+    </div>
+  );
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,22 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import "tailwindcss/tailwind.css";
+import type { AppProps } from "next/app";
+import { Layout } from "../components/Layout";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>HighFlyers</title>
+        <meta name="HighFlyers" content="Scientific Organization High Flyers" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
