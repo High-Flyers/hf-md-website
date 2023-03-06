17e4f05c5dcfecf3c193421f89945e804fc78da4
Jacek Grzybowski <totospl99@gmail.com>
2023-03-06 15:46:32 +0100

    mobile posts

--- components/post-preview.tsx
+++ components/post-preview.tsx
@@ -1,41 +1,35 @@
-import Avatar from "./avatar";
 import DateFormatter from "./date-formatter";
 import CoverImage from "./cover-image";
 import Link from "next/link";
-import type Author from "../interfaces/author";
 
 type Props = {
   title: string;
   coverImage: string;
   date: string;
   excerpt: string;
-  author: Author;
   slug: string;
 };
 
-const PostPreview = ({
-  title,
-  coverImage,
-  date,
-  excerpt,
-  author,
-  slug,
-}: Props) => {
+const PostPreview = ({ title, coverImage, date, excerpt, slug }: Props) => {
   return (
-    <div>
+    <div className="bg-my-postshow relative">
       <div className="mb-5">
         <CoverImage slug={slug} title={title} src={coverImage} />
       </div>
-      <h3 className="text-3xl mb-3 leading-snug">
+      <h3 className="text-xl font-saira m-3 leading-snug">
         <Link as={`/posts/${slug}`} href="/posts/[slug]">
           <a className="hover:underline">{title}</a>
         </Link>
       </h3>
-      <div className="text-lg mb-4">
+      <div className="text-lg m-3 mt-4">
         <DateFormatter dateString={date} />
       </div>
-      <p className="text-lg leading-relaxed mb-4">{excerpt}</p>
-      <Avatar name={author.name} picture={author.picture} />
+      <p className="text-lg leading-relaxed m-3 mb-16 font-saira">{excerpt}</p>
+      <Link as={`/posts/${slug}`} href="/posts/[slug]">
+        <div className="absolute bottom-0 bg-my-button h-10 w-1/2 font-saira text-md m-3 text-white text-center hover:cursor-pointer">
+          <p className="inline-block align-middle mt-2">CZYTAJ DALEJ</p>
+        </div>
+      </Link>
     </div>
   );
 };
