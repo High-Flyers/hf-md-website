98769816902d52216fb356a9a6bc2250d375b889
Jacek Grzybowski <totospl99@gmail.com>
2023-03-10 01:17:06 +0100

    fixed post preview

--- components/post-preview.tsx
+++ components/post-preview.tsx
@@ -12,24 +12,34 @@ type Props = {
 
 const PostPreview = ({ title, coverImage, date, excerpt, slug }: Props) => {
   return (
-    <div className="bg-my-postshow relative">
-      <div className="mb-5">
-        <CoverImage slug={slug} title={title} src={coverImage} />
-      </div>
-      <h3 className="text-xl font-saira m-3 leading-snug">
+    <div className="bg-my-postshow relative h-full flex flex-col justify-between">
+      <div>
         <Link as={`/posts/${slug}`} href="/posts/[slug]">
-          <a className="hover:underline">{title}</a>
+          <div className="mb-5 h-[200px] overflow-hidden">
+            <img
+              src={coverImage}
+              alt={`Cover Image for ${title}`}
+              className="block h-full w-full object-cover object-center hover:scale-105 transition duration-150"
+            />
+          </div>
         </Link>
-      </h3>
-      <div className="text-lg m-3 mt-4">
-        <DateFormatter dateString={date} />
-      </div>
-      <p className="text-lg leading-relaxed m-3 mb-16 font-saira">{excerpt}</p>
-      <Link as={`/posts/${slug}`} href="/posts/[slug]">
-        <div className="absolute bottom-0 bg-my-button h-10 w-1/2 font-saira text-md m-3 text-white text-center hover:cursor-pointer">
-          <p className="inline-block align-middle mt-2">CZYTAJ DALEJ</p>
+        <div className="flex flex-col gap-y-3 mx-3">
+          <h3 className="text-xl font-saira leading-snug">
+            <Link as={`/posts/${slug}`} href="/posts/[slug]">
+              <a className="hover:underline">{title}</a>
+            </Link>
+          </h3>
+          <div className="text-lg ">
+            <DateFormatter dateString={date} />
+          </div>
+          <p className="text-lg leading-relaxed  font-saira">{excerpt}</p>
         </div>
-      </Link>
+      </div>
+      <button className="block m-3 bg-my-button self-start font-saira text-md p-3 text-white text-center hover:cursor-pointer ">
+        <Link as={`/posts/${slug}`} href="/posts/[slug]">
+          CZYTAJ DALEJ
+        </Link>
+      </button>
     </div>
   );
 };
