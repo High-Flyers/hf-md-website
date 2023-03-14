3a163b9974cb227b7cd922016841b4dc0a57bb17
Jacek Grzybowski <totospl99@gmail.com>
2023-03-14 14:35:05 +0100

    added sponor panel

--- components/sponsors.tsx
+++ components/sponsors.tsx
@@ -1,31 +1,46 @@
 import SponsorImage from "./sponsor-image";
+import SmallHeader from "./utils/small-header";
 
 type Props = {};
 
-export const Sponsors = ({ }: Props) => {
+export const Sponsors = ({}: Props) => {
   return (
     <div className="lg:w-4/6 lg:mx-auto lg:my-auto">
-      <h1 className="text-3xl md:text-5xl font-bold mb-2">Sponsorzy</h1>
-      <div className="p-8 m-8 bg-bg-beubl rounded-2xl border-4 border-white grid justify-items-center content-around grid-cols-1 md:grid-cols-2 gap-8">
+      <SmallHeader name="Sponsorzy" />
+      <p className="font-base text-base mt-4">
+        Nasze koło nie mogłoby rozwijać się bez wsparcia finansowego i
+        sprzętowego naszych sponsorów.
+      </p>
+
+      <div className="mt-4 w-8/12 mx-auto">
         <SponsorImage
           src="/sponsors/ft.png"
           alt="Flytronic"
           link="https://www.wbgroup.pl/flytronic/"
+          small={false}
         />
+        <h2 className="font-special text-xl font-bold text-center">
+          SPONSOR STRATEGICZNY
+        </h2>
+      </div>
+      <div className="p-8 m-8 bg-bg-beubl rounded-2xl border-4 border-white grid justify-items-center content-around grid-cols-1 xl:grid-cols-3 gap-8">
         <SponsorImage
           src="/sponsors/logo-dron.png"
           alt="DronEdu"
           link="https://dron.edu.pl"
+          small={true}
+        />
+        <SponsorImage
+          src="/sponsors/polsl.png"
+          alt="PolSl"
+          link="https://www.polsl.pl"
+          small={true}
         />
         <SponsorImage
           src="/sponsors/botland.png"
           alt="Botland"
           link="https://botland.com.pl"
-        />
-        <SponsorImage
-          src="/sponsors/uzss.png"
-          alt="UZSS"
-          link="https://www.polsl.pl/ribek/samorzad-studencki"
+          small={true}
         />
       </div>
     </div>
---
+++ components/utils/small-header.tsx
@@ -0,0 +1,14 @@
+type Props = {
+  name: string;
+};
+
+export const SmallHeader = ({ name }: Props) => {
+  return (
+    <div className="font-special  text-xl md:text-2xl lg:text-xl w-fit mr-auto">
+      <h3 className="text-center w-fit mx-auto">{name}</h3>
+      <div className="h-accent-sm w-full bg-my-contrast mx-auto" />
+    </div>
+  );
+};
+
+export default SmallHeader;
