17cae2e0dd3468fd0e6256b368729eea7069652b
Jacek Grzybowski <totospl99@gmail.com>
2023-03-17 19:34:35 +0100

    adjusting to page size

--- components/utils/main-title.tsx
+++ components/utils/main-title.tsx
@@ -1,19 +1,26 @@
 type Props = {
-    name: string;
-    className?: string;
-    classNameUnderline?: string;
+  name: string;
+  className?: string;
+  classNameUnderline?: string;
 };
 
 const MainTitle = ({ name, className, classNameUnderline }: Props) => {
-    return (
-        <div className={"font-additional font-bold text-sm mx-auto text-xl w-fit \
-                         md:text-base m:text-xl md:text-2xl lg:text-4xl " + className}>
-            <h3 className="text-center w-fit mx-auto">
-                {name}
-            </h3>
-            <div className={"h-accent-sm w-1/2 bg-my-contrast mt-[-0.2em] mx-auto " + classNameUnderline} />
-        </div>
-    );
+  return (
+    <div
+      className={
+        "font-additional font-bold text-xl mx-auto w-fit md:text-2xl lg:text-4xl " +
+        className
+      }
+    >
+      <h3 className="text-center w-fit mx-auto">{name}</h3>
+      <div
+        className={
+          "h-accent-sm w-1/2 bg-my-contrast mt-[-0.2em] mx-auto " +
+          classNameUnderline
+        }
+      />
+    </div>
+  );
 };
 
 export default MainTitle;
